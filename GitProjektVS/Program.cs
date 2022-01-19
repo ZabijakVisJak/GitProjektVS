f7661e5601816ae0eccd52a6bdf6ab5e090446ff
Planka <planka@netoffice.cz>
2022-01-19 12:02:09 +0100

    Přidejte soubory projektu.

---
+++ GitProjektVS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitProjektVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
